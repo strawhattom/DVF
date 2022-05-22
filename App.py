70e22765354b92d5857ba9aafc12d78647103573
Tom Xie <xie.tom7@gmail.com>
2022-05-22 23:04:37 +0200

    button working

--- App.py
+++ App.py
@@ -2,9 +2,49 @@ import io
 from flask import Flask, url_for, redirect, render_template, request, make_response, Response, send_file
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from covid import covidFig
-from datatemplate import data
 
 app = Flask(__name__)
+
+data = {}
+for i in range(1,5):
+    data[i] = {
+        "figure":"NULL",
+        "headers":"NULL",
+        'name':"NULL",
+        'description':"NULL",
+        "data":"NULL",
+    }
+
+# First plot
+data[1]["figure"] = "plot"
+data[1]["name"] = "Covid"
+data[1]["description"] = "Nombre de cas de la covid"
+
+# Second plot
+data[2]["figure"] = "plot"
+data[2]["name"] = "Nombre de vente"
+data[2]["description"] = "Par département"
+
+# Third
+data[3]["figure"] = "plot"
+data[3]["name"] = "Prix surface"
+data[3]["description"] = "Par région"
+
+
+data[4]["figure"] = "plot"
+data[4]["name"] = "Différence de prix et des ventes en haut de france"
+data[4]["description"] = "Par région"
+
+regionList = [
+    "metropole",
+    "RhoneAlpes",
+    "HautDeFrance"
+]
+
+yearList = [
+    "2018",
+    "2021",
+]
 
 @app.route('/')
 def home():
@@ -21,34 +61,33 @@ def createPlot(id):
         FigureCanvas(covidFig).print_png(output)
         return Response(output.getvalue(), mimetype='image/png')
     elif id == 2 :
-        # nb vente
-        # region = request.args.get('region', default = "metropole", type = str)
-        # year = request.args.get('year', default = "2021", type = str)
-        # fig = nvFigures2020[region] if year == "2020" else nvFigures2021[region]
-        # output = io.BytesIO()
-        # FigureCanvas(fig).print_png(output)
-        # return Response(output.getvalue(), mimetype='image/png')
 
         region = request.args.get('region', default = "metropole", type = str)
         region = "metropole" if region != "RhoneAlpes" else "RhoneAlpes"
         year = 2021
-        return send_file("/static/Ventes{region}{year}.png", mimetype="image/png")
+        return send_file(f"static\\Ventes{region}{year}.png", mimetype="image/png")
     elif id == 3 :
         # prix surface
         # output = io.BytesIO()
         region = request.args.get('region', default = "Occitanie", type = str)
-        # year = request.args.get('year', default = "2021", type = str)
-        year = 2021
-        # fig = psFigures2020[region] if year == "2020" else nvFigures2021[region]
-        # FigureCanvas(fig).print_png(output)
-        # return Response(output.getvalue(), mimetype='image/png')   
-        return send_file(f"/static/Ventes", mimetype="image/png")
+        region = "Occitanie" if region != "RhoneAlpes" else "RhoneAlpes"
+        return send_file(f"static\\PrixSurface{region}.png", mimetype="image/png")
     elif id == 4:
-        region = request.args.get('region', default = "HautDeFrance", type = str)
-        send_file(f"/static/DiffVentes{region}.png", mimetype="img/png")
-        return send_file(f"/static/DiffPrix{region}.png", mimetype="img/png")
+        year = request.args.get('year', default = "2021", type = str)
+        year = "2018" if year == "2018" else "2021"
+        region = "HautDeFrance"
+        return send_file(f"static\\DiffPrix{year}{region}.png", mimetype="img/png")
 
     else: return NULL
+
+@app.route('/<int:id>/plot2.png')
+def createPlot2(id):
+    if id == 4:
+        year = request.args.get('year', default = "2021", type = str)
+        year = "2021" if year == "2021" else "2018"
+        region = "HautDeFrance"
+        return send_file(f"static\\DiffVentes{year}{region}.png", mimetype="img/png")
+    else : return NULL
 
 @app.route('/<int:id>')
 def visualization(id):
@@ -60,11 +99,13 @@ def visualization(id):
     return render_template(
         'visualization.html', # template name
         title = f'Visualization {id}',
-        id = id, 
+        id = id,
+        yearList = yearList,
+        regionList = regionList,
         year = year,
         region = region,
         name = data[id]["name"], 
-        desc = data[id]["description"],
+        desc = data[id]["description"] + f" {year}" + f", {region}",
         figure = data[id]["figure"], 
         table = data[id] 
     )
