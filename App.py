7a6cfe6f16a7a49c6a8e7d91de65c11af9916e72
tomx93 <xie.tom7@gmail.com>
2022-05-22 14:03:16 +0200

    plot with params

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import io
 from flask import Flask, url_for, redirect, render_template, request, make_response, Response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from covid import covidFig
-from dataframe import data, venteFig
+from dataframe import *
 
 app = Flask(__name__)
 
@@ -21,9 +21,21 @@ def createPlot(id):
         FigureCanvas(covidFig).print_png(output)
         return Response(output.getvalue(), mimetype='image/png')
     elif id == 2 :
+        # nb vente
+        region = request.args.get('region', default = "metropole", type = str)
+        year = request.args.get('year', default = "2021", type = str)
+        fig = nvFigures2020[region] if year == "2020" else nvFigures2021[region]
         output = io.BytesIO()
-        FigureCanvas(venteFig).print_png(output)
+        FigureCanvas(fig).print_png(output)
         return Response(output.getvalue(), mimetype='image/png')
+    elif id == 3 :
+        # prix surface
+        output = io.BytesIO()
+        region = request.args.get('region', default = "metropole", type = str)
+        year = request.args.get('year', default = "2021", type = str)
+        fig = psFigures2020[region] if year == "2020" else nvFigures2021[region]
+        FigureCanvas(fig).print_png(output)
+        return Response(output.getvalue(), mimetype='image/png')      
     else: return NULL
 
 @app.route('/<int:id>')
@@ -31,13 +43,14 @@ def visualization(id):
     if (id < 1 or id > 4):
         return redirect(url_for('home'))
 
-    city = request.args.get('city', default = 'paris', type = int)
-
+    region = request.args.get('region', default = "metropole", type = str)
+    year = request.args.get('year', default = "2021", type = str)
     return render_template(
         'visualization.html', # template name
         title = f'Visualization {id}',
         id = id, 
-        city = city, 
+        year = year,
+        region = region,
         name = data[id]["name"], 
         desc = data[id]["description"],
         figure = data[id]["figure"], 
--- dataframe.py
+++ dataframe.py
@@ -5,69 +5,162 @@ import numpy as np
 from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 
 def importData():
-    df = pd.read_csv("static/valeursfoncieres-2021.txt",sep="|")
+
+    df2021 = pd.read_csv("static/valeursfoncieres-2021.txt",sep="|")
+    df2020 = pd.read_csv("static/valeursfoncieres-2020.txt",sep="|")
+
     # Remove not usable column
-    nbcolumn=df.shape[0]
+    nbcolumn2020=df2020.shape[0]
+    nbcolumn2021=df2021.shape[0]
+
     prc_Nan=0.75
-    max_number_of_nas = nbcolumn * prc_Nan
-    dfclean = df.loc[:, (df.isnull().sum(axis=0) <= max_number_of_nas)]
+    max_number_of_nas_2020 = nbcolumn2020 * prc_Nan
+    max_number_of_nas_2021 = nbcolumn2021 * prc_Nan
 
-    # Clean column "valeur foncière"
-    dfclean["Valeur fonciere"] = dfclean["Valeur fonciere"].replace(',','.',regex=True).astype(float)
-    dfclean["Valeur fonciere"].dtype
-    return dfclean
+    clean2020 = df2020.loc[:, (df2020.isnull().sum(axis=0) <= max_number_of_nas_2020)]
+    clean2021 = df2021.loc[:, (df2021.isnull().sum(axis=0) <= max_number_of_nas_2021)]
 
-def prixSurface(df):
-    tmp = df.groupby('Code departement').mean().reset_index()
-    list_prixSurface = [tmp["Code departement"] , tmp["prix/m^2"]]
-    header = ["departement" , "prix/m^2"]
-    df_prixSurface = pd.concat(list_prixSurface, axis=1, keys=header)
+    # Exploit data
+    clean2020["Valeur fonciere"]=clean2020["Valeur fonciere"].replace(',','.',regex=True).astype(float)
+    clean2020["prix/m^2"] = clean2020["Valeur fonciere"] / clean2020["Surface reelle bati"]
+    clean2020["prix/m^2"] = clean2020["prix/m^2"].apply(lambda x: x if x!=np.inf else 0)
+    clean2020["compte"]=1
 
-    df_prixSurface = df_prixSurface.drop([91,92,93,94,95,98,49,37])
-    df_prixSurface["departement"]=df_prixSurface["departement"].apply(lambda x : str(0) + str(x) if len(str(x))==1 else str(x))
-    df_new_line = pd.DataFrame([['67',np.nan] , ['68',np.nan] , ['57',np.nan], ['49',np.nan],[37,np.nan]] , columns=['departement','prix/m^2'])
-    df_prixSurface = pd.concat([df_prixSurface , df_new_line] , ignore_index=True)
-    return df_prixSurface
+    clean2021["Valeur fonciere"]=clean2021["Valeur fonciere"].replace(',','.',regex=True).astype(float)
+    clean2021["prix/m^2"] = clean2021["Valeur fonciere"] / clean2021["Surface reelle bati"]
+    clean2021["prix/m^2"] = clean2021["prix/m^2"].apply(lambda x: x if x!=np.inf else 0)
+    clean2021["compte"]=1
+    return clean2020, clean2021
 
-def venteParDepartement(df):
-    #creation table
-    df["compte"]=1
-    tmp2=df.groupby('Code departement').sum().reset_index()
-    list_departement_nbventes=[tmp2["Code departement"],tmp2["compte"]]
-    header=["departement","nombre de ventes"]
-    df_nbventes=pd.concat(list_departement_nbventes, axis=1, keys=header)
+def prixSurface(clean2020, clean2021):
+    PrixSurface2020 = clean2020.groupby('Code departement').mean().reset_index().drop(columns = ['No disposition','No voie', 'Code postal', 'Code commune', 'No plan','Nombre de lots'
+                                                                                            , 'Code type local', 'Nombre pieces principales'], axis = 1)
+    PrixSurface2021 = clean2021.groupby('Code departement').mean().reset_index().drop(columns = ['No disposition','No voie', 'Code postal', 'Code commune', 'No plan','Nombre de lots'
+                                                                                                , 'Code type local', 'Nombre pieces principales'], axis = 1)
 
-    #clean table
-    df_nbventes=df_nbventes.drop([91,92,93,94,95,98])
-    df_nbventes["departement"]=df_nbventes["departement"].apply(lambda x : str(0) + str(x) if len(str(x))==1 else str(x))
-    df_new_line = pd.DataFrame([['67',np.nan],['68',np.nan],['57',np.nan]], columns=['departement','nombre de ventes'])
-    df_nbventes = pd.concat([df_nbventes,df_new_line], ignore_index=True)
-    return df_nbventes
+    PrixSurface2020["Code departement"]=PrixSurface2020["Code departement"].apply(lambda x : str(0) + str(x) if len(str(x))==1 else str(x))
+    df_new_line = pd.DataFrame([['67',np.nan] , ['68',np.nan] , ['57',np.nan]] , columns=['Code departement','prix/m^2'])
+    PrixSurface2020 = pd.concat([PrixSurface2020 , df_new_line] , ignore_index=True)
+    PrixSurface2020.drop([95,98])
 
-def departement():
+    PrixSurface2021["Code departement"]=PrixSurface2021["Code departement"].apply(lambda x : str(0) + str(x) if len(str(x))==1 else str(x))
+    df_new_line = pd.DataFrame([['67',np.nan] , ['68',np.nan] , ['57',np.nan]] , columns=['Code departement','prix/m^2'])
+    PrixSurface2021 = pd.concat([PrixSurface2021 , df_new_line] , ignore_index=True)
+    PrixSurface2021.drop([95,98])
+
+    PrixSurfaceAnnee = {
+        "2020" : PrixSurface2020,
+        "2021" : PrixSurface2021
+        }
+
+    return PrixSurfaceAnnee
+
+def region(geo):
+    # Region
+
     codes = [_ for _ in set(geo.code) if len(_) < 3]
     metropole = geo[geo.code.isin(codes)]
-    return metropole
 
-def venteFigure(df):
-    metropole = departement()
-    merged2 =  venteParDepartement(df).reset_index(drop=False).merge(metropole, left_on="departement", right_on="code")
-    merged2.shape
-    geomergedNbVentes = gpd.GeoDataFrame(merged2)
+    Paris= metropole.drop(metropole[(metropole.code!="75") & (metropole.code!="77") & (metropole.code!="78") & (metropole.code!="91") & (metropole.code!="92") & (metropole.code!="93") & (metropole.code!="94") & (metropole.code!="95")].index)
+    RhoneAlpes = metropole.drop(metropole[(metropole.code!="03") & (metropole.code!="42") & (metropole.code!="69") & (metropole.code!="01") & (metropole.code!="74") & (metropole.code!="63") & (metropole.code!="43") & (metropole.code!="15") & (metropole.code!="07")& (metropole.code!="26")& (metropole.code!="38")& (metropole.code!="73")].index)
+    Bretagne = metropole.drop(metropole[(metropole.code!="29") & (metropole.code!="22") & (metropole.code!="35") & (metropole.code!="56")].index)
+    Nouvelle_Aquitaine = metropole.drop(metropole[(metropole.code!="79") & (metropole.code!="86") & (metropole.code!="87") & (metropole.code!="23") & (metropole.code!="17") & (metropole.code!="16") & (metropole.code!="24") & (metropole.code!="33") & (metropole.code!="47")& (metropole.code!="40")& (metropole.code!="64") & (metropole.code!="19")].index)
+    Occitanie = metropole.drop(metropole[(metropole.code!="46") & (metropole.code!="12") & (metropole.code!="48") & (metropole.code!="30") & (metropole.code!="82") & (metropole.code!="81") & (metropole.code!="34") & (metropole.code!="32") & (metropole.code!="31")& (metropole.code!="65")& (metropole.code!="09")& (metropole.code!="11")& (metropole.code!="66")].index)
+    ProvenceAlpes_coteDAzure= metropole.drop(metropole[(metropole.code!="05") & (metropole.code!="04") & (metropole.code!="06") & (metropole.code!="84") & (metropole.code!="13") & (metropole.code!="83")].index)
+    Corse = metropole.drop(metropole[(metropole.code != '2B') & (metropole.code != "2A")].index)
+    Bourgogne_FrancheCompte = metropole.drop(metropole[(metropole.code!="89") & (metropole.code!="90") & (metropole.code!="21") & (metropole.code!="70") & (metropole.code!="58") & (metropole.code!="71") & (metropole.code!="39") & (metropole.code!="25")].index)
+    Grand_Est = metropole.drop(metropole[(metropole.code!="08") & (metropole.code!="55") & (metropole.code!="57") & (metropole.code!="54") & (metropole.code!="67") & (metropole.code!="51") & (metropole.code!="10") & (metropole.code!="52") & (metropole.code!="88")& (metropole.code!="68")].index)
+    Centre_ValDeLoire = metropole.drop(metropole[(metropole.code!="28") & (metropole.code!="45") & (metropole.code!="41") & (metropole.code!="37") & (metropole.code!="36") & (metropole.code!="18")  ].index)
+    Pays_DeLaLoire = metropole.drop(metropole[(metropole.code!="53") & (metropole.code!="72") & (metropole.code!="49") & (metropole.code!="44") & (metropole.code!="85")].index)
+    Normandie = metropole.drop(metropole[(metropole.code!="76") & (metropole.code!="27") & (metropole.code!="14") & (metropole.code!="50") & (metropole.code!="61")].index)
+    Hauts_DeFrance = metropole.drop(metropole[(metropole.code!="62") & (metropole.code!="59") & (metropole.code!="80") & (metropole.code!="60") & (metropole.code!="02")].index)
+
+    # Dico de Region
+    Region = {
+            "Paris" : Paris,
+            "RhoneAlpes" : RhoneAlpes,
+            "Bretagne" : Bretagne,
+            "Nouvelle_Aquitaine" : Nouvelle_Aquitaine,
+            "Occitanie" : Occitanie,
+            "ProvenceAlpes_coteDAzure" : ProvenceAlpes_coteDAzure,
+            "Corse" : Corse,
+            "Bourgogne_FrancheCompte" : Bourgogne_FrancheCompte,
+            "Grand_Est" : Grand_Est,
+            "Centre_ValDeLoire" : Centre_ValDeLoire,
+            "Pays_DeLaLoire" : Pays_DeLaLoire,
+            "Normandie" : Normandie,
+            "Hauts_DeFrance" : Hauts_DeFrance,
+            "metropole" : metropole
+        }
+    return Region
+
+def venteDepartement(clean2020, clean2021):
+    #creation table
+    NmbVentes2020 = clean2020.groupby('Code departement').sum().reset_index().drop(columns = ['No disposition','No voie', 'Code postal', 'Code commune', 'No plan','Nombre de lots'
+                                                                                            , 'Code type local', 'Nombre pieces principales'], axis = 1)
+    NmbVentes2021 = clean2021.groupby('Code departement').sum().reset_index().drop(columns = ['No disposition','No voie', 'Code postal', 'Code commune', 'No plan','Nombre de lots'
+                                                                                                , 'Code type local', 'Nombre pieces principales'], axis = 1)
+
+    NmbVentes2020["Code departement"] = NmbVentes2020["Code departement"].apply(lambda x : str(0) + str(x) if len(str(x))==1 else str(x))
+    df_new_line = pd.DataFrame([['67',0] , ['68',0] , ['57',0]] , columns=['Code departement','compte'])
+    NmbVentes2020 = pd.concat([NmbVentes2020 , df_new_line] , ignore_index=True)
+    NmbVentes2020.drop([95,98])
+
+    NmbVentes2021["Code departement"] = NmbVentes2021["Code departement"].apply(lambda x : str(0) + str(x) if len(str(x))==1 else str(x))
+    df_new_line = pd.DataFrame([['67',0] , ['68',0] , ['57',0]] , columns=['Code departement','compte'])
+    NmbVentes2021 = pd.concat([NmbVentes2021 , df_new_line] , ignore_index=True)
+    NmbVentes2021.drop([95,98])
+    NmbVentesAnnee = {"2020" : NmbVentes2020,
+                  "2021" : NmbVentes2021}
+    return NmbVentesAnnee
+
+def figureNmbVentes(NmbVentesAnnee, Region, annee = "2020", nom_region = "metropole"):
+    merged =  NmbVentesAnnee[anee].reset_index(drop=False).merge(Region[nom_region], left_on="Code departement", right_on="code")
+    merged.shape
+    geomerged = gpd.GeoDataFrame(merged)
     fig, ax = plt.subplots(1, 1, figsize=(16, 6))
 
     cax = make_axes_locatable(ax).append_axes("right", size="5%", pad=0.1)
 
-    geomergedNbVentes.plot(column="nombre de ventes", ax=ax, edgecolor='black', legend=True, cax=cax,missing_kwds={'color': 'lightgrey'})
-    ax.set_title("Nombre de ventes par departements");
-    
-    # when not using notebook
-    # plt.show()
+    geomerged.plot(column="compte", ax=ax, edgecolor='black', legend=True, cax=cax,missing_kwds={'color': 'lightgrey'})
+    ax.set_title("Nombre de ventes dans dans : " + nom_region);
+
+
+def figurePrixSurface(PrixSurfaceAnnee, Region, annee = "2020", nom_region = "metropole"):
+    merged =  PrixSurfaceAnnee[annee].reset_index(drop=False).merge(Region[nom_region], left_on="Code departement", right_on="code")
+    merged.shape
+    geomerged = gpd.GeoDataFrame(merged)
+    fig, ax = plt.subplots(1, 1, figsize=(16, 6))
+
+    cax = make_axes_locatable(ax).append_axes("right", size="5%", pad=0.1)
+
+    geomerged.plot(column="prix/m^2", ax=ax, edgecolor='black', legend=True, cax=cax,missing_kwds={'color': 'lightgrey'})
+    ax.set_title("Prix par unité de surface dans : " + nom_region);
     return fig
 
+# DF
+
 geo = gpd.read_file('https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-version-simplifiee.geojson')
-df = importData()
-venteFig = venteFigure(df)
+df_2020, df_2021 = importData()
+df_covid = pd.read_csv("static/synthese-fra.csv")
+
+r = region(geo)
+
+prixSurface = prixSurface(df_2020, df_2021) # prixSurface["2021"], prixSurface["2022"]
+psFigures2020 = {
+    k:figurePrixSurface(prixSurface, r, "2020", r[k]) for k in r.keys()
+}
+psFigures2021 = {
+    k:figurePrixSurface(prixSurface, r, "2021", r[k]) for k in r.keys()
+}
+
+nmbVentesAnnee = venteDepartement(df_2020, df_2021)
+nvFigures2020 = {
+    k:figureNmbVentes(nmbVentesAnnee, r, "2020", r[k]) for k in r.keys()
+}
+nvFigures2021 = {
+    k:figureNmbVentes(nmbVentesAnnee, r, "2021", r[k]) for k in r.keys()
+}
 
 data = {}
 for i in range(1,5):
@@ -88,3 +181,8 @@ data[1]["description"] = "Nombre de cas de la covid"
 data[2]["figure"] = "plot"
 data[2]["name"] = "Nombre de vente"
 data[2]["description"] = "Par département"
+
+# Third
+data[3]["figure"] = "plot"
+data[3]["name"] = "Prix surface"
+data[3]["description"] = "Par région"
